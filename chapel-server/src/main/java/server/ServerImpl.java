1e97c5a1b183ffb260a187fb3ac3338f0d1026c5
ramazan <rmzs0711@yandex.ru>
2022-05-20 19:30:53 +0300

    simple code lens works

--- chapel-server/src/main/java/server/ServerImpl.java
+++ chapel-server/src/main/java/server/ServerImpl.java
@@ -1,13 +1,24 @@
 package server;
+import com.google.gson.Gson;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonPrimitive;
 import org.eclipse.lsp4j.*;
+import org.eclipse.lsp4j.jsonrpc.json.adapters.TypeUtils;
 import org.eclipse.lsp4j.services.*;
 
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.*;
 import java.util.concurrent.CompletableFuture;
 import java.util.logging.Logger;
+
+import static server.ServerConstants.InsertVarTypeCommand;
 
 public class ServerImpl implements LanguageServer, LanguageClientAware {
     private static final Logger LOG = Logger.getLogger("server");
     private LanguageClient client = null;
+    private Gson gson = new Gson();
     @Override
     public CompletableFuture<InitializeResult> initialize(InitializeParams params) {
         ServerCapabilities capabilities = new ServerCapabilities();
@@ -17,10 +28,20 @@ public class ServerImpl implements LanguageServer, LanguageClientAware {
         capabilities.setDeclarationProvider(false);
         capabilities.setDefinitionProvider(false);
         capabilities.setHoverProvider(false);
+
+        var codeLensOptions = new CodeLensOptions();
+        codeLensOptions.setWorkDoneProgress(false);
+        codeLensOptions.setResolveProvider(true);
+        capabilities.setCodeLensProvider(codeLensOptions);
+
+        var execOptions = new ExecuteCommandOptions();
+        execOptions.setCommands(Collections.singletonList(InsertVarTypeCommand));
+        capabilities.setExecuteCommandProvider(execOptions);
+
 //        var provider = new SemanticTokensWithRegistrationOptions();
 
 //        capabilities.setSemanticTokensProvider();
-        LOG.info("exiting");
+        LOG.info("Initialized");
         return CompletableFuture.completedFuture(new InitializeResult(capabilities));
     }
 
@@ -45,6 +66,28 @@ public class ServerImpl implements LanguageServer, LanguageClientAware {
             }
 
             @Override
+            public CompletableFuture<List<? extends CodeLens>> codeLens(CodeLensParams params) {
+                List<CodeLens> list = new ArrayList<>();
+
+                CodeLens lens = new CodeLens();
+                lens.setRange(new Range(new Position(0, 0), new Position(0, 10)));
+
+                lens.setCommand(new Command(
+                        "title",
+                        InsertVarTypeCommand,
+                        Collections.singletonList(params.getTextDocument().getUri())));
+                list.add(lens);
+
+                return CompletableFuture.completedFuture(list);
+            }
+
+            @Override
+            public CompletableFuture<CodeLens> resolveCodeLens(CodeLens unresolved) {
+                LOG.info(unresolved.toString());
+                return CompletableFuture.completedFuture(unresolved);
+            }
+
+            @Override
             public void didChange(DidChangeTextDocumentParams params) {
 
             }
@@ -66,12 +109,38 @@ public class ServerImpl implements LanguageServer, LanguageClientAware {
         return new WorkspaceService() {
             @Override
             public void didChangeConfiguration(DidChangeConfigurationParams params) {
-
             }
 
             @Override
             public void didChangeWatchedFiles(DidChangeWatchedFilesParams params) {
+                connect(null);
+            }
 
+            @Override
+            public CompletableFuture<Object> executeCommand(ExecuteCommandParams params) {
+                ApplyWorkspaceEditParams applyWorkspaceEditParams = new ApplyWorkspaceEditParams();
+                WorkspaceEdit workspaceEdit = new WorkspaceEdit();
+                var uri = ((JsonElement)params.getArguments().get(0)).getAsString();
+
+                LOG.info(uri);
+                // todo
+                Map<String, List<TextEdit>> stringListMap = new HashMap<>();
+
+                stringListMap.put(uri, Collections.singletonList(new TextEdit(
+                        new Range(new Position(0, 0), new Position(0, 1)), "gotcha"
+                )));
+                workspaceEdit.setChanges(stringListMap);
+
+                applyWorkspaceEditParams.setEdit(workspaceEdit);
+
+                client.applyEdit(applyWorkspaceEditParams);
+                switch (params.getCommand()){
+                    case InsertVarTypeCommand:
+                        break;
+                    default:
+                        LOG.info("unknown command: " + params.getCommand());
+                }
+                return CompletableFuture.completedFuture("");
             }
         };
     }
